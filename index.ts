5cb9568d48c1e8cfbe8e6022f1991397929e99b7
Minh Dat <ngominhdat2004@gmail.com>
2026-05-08 01:55:28 +0700

    1. Lấy ra danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("oke");
+import express, { Express, Request, Response } from "express";
+import * as database from "./config/database";
+import dotenv from "dotenv";
+import Task from "./api/v1/models/task.model";
+
+dotenv.config();
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server is running on port ${port} 🚀`);
+});
